5a6835ae435ac17acebfe9c856909663a12a2859
helmbreaker <vigneshataraku@gmail.com>
2017-08-08 12:39:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,15 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/Articlesecond' function (req,res){
+    res.send('This is the second article that i have typed today');
+});
+app.get('/Articleone' function (req,res){
+    res.send('this is the first article i am going to type ');
+});
+app.get('/Articlethree' function (req,res){
+    res.send('This is the final scream!')
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
